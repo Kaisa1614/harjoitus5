5f5ba4a7438ad0f2002056bcdc7c995795400b64
Kaisa1614 <35688012+Kaisa1614@users.noreply.github.com>
2018-02-07 21:07:37 +0200

    Initial commit

---
+++ MyTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace harjoitus5
+{
+    [TestFixture]
+
+    class MyTest
+    {
+        [TestCase]
+        public void Add()
+        {
+            Math math = new Math();
+            Assert.AreEqual(31, math.Add(20, 11));
+        }
+
+        [TestCase]
+        public void sub()
+        {
+            Math math = new Math();
+            Assert.AreEqual(10, math.Sub(20, 10));
+        }
+
+    }
+}
